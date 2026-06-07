12c8b99cf1189e8ccbb2d01e79331ef5e34c9dd4
Ayush1Rajput <ayushraj1rajput@gmail.com>
2026-06-07 23:01:11 +0530

    feat: Add createTask component

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,38 +1,15 @@
 import React from "react";
-import Header from "../other/Header";
+import Header from '../other/Header'
+import CreateTask from "../other/CreateTask";
 
 const AdminDashboard = () => {
   return (
-    <div className="h-screen w-full p-10">
+    <div className='h-screen w-full p-7'>
       <Header />
-
-      <div>
-        <form className="flex items-start justify-between">
-          <div>
-            <h3>Task Title</h3>
-            <input type="text" placeholder="Make a UI" />
-          </div>
-          <div>
-            <h3>Description</h3>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-          <div>
-            <h3>Date</h3>
-            <input type="date" name="" id="" />
-          </div>
-          <div>
-            <h3>Assign to </h3>
-            <input type="text" placeholder="Employee Name" />
-          </div>
-          <div>
-            <h3>Category</h3>
-            <input type="text" />
-          </div>
-            <button>Create Task</button>
-        </form>
-      </div>
+      <CreateTask/>
+      
     </div>
-  );
-};
+  )
+}
 
-export default AdminDashboard;
+export default AdminDashboard

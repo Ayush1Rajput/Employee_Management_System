d56a1a9be55e80b19cff841a7f1a57fe422d82ac
Ayush1Rajput <ayushraj1rajput@gmail.com>
2026-06-07 14:29:27 +0530

    feat: Add taskListNumber component

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../other/Header'
+import TaskListNumber from '../other/TaskListNumber'
 
 function EmployeeDashboard() {
   return (
     <div>
         <div className='p-10 bg-[#1c1c1c] h-screen'>
             <Header/>
+            <TaskListNumber/>
         </div>
     </div>
   )

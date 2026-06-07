b0c9d5696596543b3263fe918288c2d67cd48d84
Ayush1Rajput <ayushraj1rajput@gmail.com>
2026-06-07 14:47:19 +0530

    feat: Add TaskList component and add in dashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskListNumber from '../other/TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
 function EmployeeDashboard() {
   return (
@@ -8,6 +9,7 @@ function EmployeeDashboard() {
         <div className='p-10 bg-[#1c1c1c] h-screen'>
             <Header/>
             <TaskListNumber/>
+            <TaskList/>
         </div>
     </div>
   )

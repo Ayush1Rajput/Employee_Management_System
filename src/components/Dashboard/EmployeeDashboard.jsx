c535c94d3e7c63f15016d5c2ec5fa5d8da577ae0
Ayush1Rajput <ayushraj1rajput@gmail.com>
2026-06-07 14:17:32 +0530

    feat: working on Employee Dashboard

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../other/Header'
+
+function EmployeeDashboard() {
+  return (
+    <div>
+        <div className='p-10 bg-[#1c1c1c] h-screen'>
+            <Header/>
+        </div>
+    </div>
+  )
+}
+
+export default EmployeeDashboard

0cd552aa37c3bb673c194b784568981a5c9a71c9
Ayush1Rajput <ayushraj1rajput@gmail.com>
2026-06-17 01:07:53 +0530

    fix: format

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { useEffect } from 'react'
+import React, {useEffect } from 'react'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 function App() {
